330e4213355ee3a76fd8822b688b98b8c04e587c
Bryton Kinney <brytonkinney@gmail.com>
2018-01-29 21:58:40 -0600

    Added DataTables to Bundle Config

--- HealthSpot/HealthSpot/App_Start/BundleConfig.cs
+++ HealthSpot/HealthSpot/App_Start/BundleConfig.cs
@@ -13,19 +13,25 @@ namespace HealthSpot
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
-
+            bundles.Add(new ScriptBundle("~/bundles/dataTables").Include(
+                        "~/Scripts/dataTables.js",
+                        "~/Scripts/jquery.dataTables.js",
+                        "~/Scripts/dataTables.bootstrap4.js",
+                        "~/Scripts/jquery.dataTables*"));
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/dataTables.css",
+                      "~/Content/jquery.dataTables.css",
+                      "~/Content/dataTables.bootstrap4.css"));
         }
     }
 }
